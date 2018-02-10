599f0aa7621c692afdc40fec002773351a858e29
idleberg <jan@idleberg.com>
2018-02-10 01:21:35 +0100

    remove version string

--- src/nsis.js
+++ src/nsis.js
@@ -1,4 +1,4 @@
-/*! prism-nsis v0.1.3 | MIT License | github.com/idleberg/prismjs-nsis */
+/*! prism-nsis | MIT License | github.com/idleberg/prismjs-nsis */
 
  Prism.languages.nsis = {
 	'comment': {
